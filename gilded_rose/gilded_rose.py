6691676a6c1bd857147814a9611f15a3edbed1e2
Mike Fletcher <mike11@mike11.karoo.co.uk>
2023-11-02 11:43:09 +0000

    Created Function For Managing Concert Quality

--- gilded_rose/gilded_rose.py
+++ gilded_rose/gilded_rose.py
@@ -13,15 +13,7 @@ class GildedRose(object):
                     if item.name != "Sulfuras, Hand of Ragnaros":
                         item.quality = item.quality - 1
             else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == concert:
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
+                self.manage_concert_quality(item)
             if item.name != "Sulfuras, Hand of Ragnaros":
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
@@ -33,5 +25,16 @@ class GildedRose(object):
                     else:
                         item.quality = item.quality - item.quality
                 else:
+                    if item.quality < 50:
+                        item.quality = item.quality + 1
+
+    def manage_concert_quality(self, item):
+        if item.quality < 50:
+            item.quality = item.quality + 1
+            if item.name == concert:
+                if item.sell_in < 11:
+                    if item.quality < 50:
+                        item.quality = item.quality + 1
+                if item.sell_in < 6:
                     if item.quality < 50:
                         item.quality = item.quality + 1
