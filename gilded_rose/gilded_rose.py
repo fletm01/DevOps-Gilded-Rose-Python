10f97e685b83abe6710a09380e6f0442687c01de
Mike Fletcher <mike11@mike11.karoo.co.uk>
2023-11-02 11:37:21 +0000

    Co-authored-by: kemiosborne <kemiosborne@users.noreply.github.com>

--- gilded_rose/gilded_rose.py
+++ gilded_rose/gilded_rose.py
@@ -1,3 +1,6 @@
+
+concert = "Backstage passes to a TAFKAL80ETC concert"
+
 class GildedRose(object):
 
     def __init__(self, items):
@@ -5,14 +8,14 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+            if item.name != "Aged Brie" and item.name != concert:
                 if item.quality > 0:
                     if item.name != "Sulfuras, Hand of Ragnaros":
                         item.quality = item.quality - 1
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                    if item.name == concert:
                         if item.sell_in < 11:
                             if item.quality < 50:
                                 item.quality = item.quality + 1
@@ -23,7 +26,7 @@ class GildedRose(object):
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
                 if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
+                    if item.name != concert:
                         if item.quality > 0:
                             if item.name != "Sulfuras, Hand of Ragnaros":
                                 item.quality = item.quality - 1
