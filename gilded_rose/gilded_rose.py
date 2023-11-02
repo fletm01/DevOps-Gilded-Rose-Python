5b025653b94379dd04e3dddc9cdfeda4f457f46e
Mike Fletcher <mike11@mike11.karoo.co.uk>
2023-11-02 13:19:10 +0000

    added += and -=

--- gilded_rose/gilded_rose.py
+++ gilded_rose/gilded_rose.py
@@ -11,11 +11,11 @@ class GildedRose(object):
             if item.name != "Aged Brie" and item.name != concert:
                 if item.quality > 0:
                     if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+                        item.quality -= 1
             else:
                 self.manage_concert_quality(item)
             if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
+                item.sell_in -= 1
             self.manage_out_of_date_items(item)
 
     def manage_out_of_date_items(self, item):
@@ -24,18 +24,18 @@ class GildedRose(object):
                 if item.name != concert:
                     if item.quality > 0:
                         if item.name != "Sulfuras, Hand of Ragnaros":
-                            item.quality = item.quality - 1
+                            item.quality -= 1
                 else:
                     item.quality = 0
             else:
                 if item.quality < 50:
-                    item.quality = item.quality + 1
+                    item.quality += 1 
 
     def manage_concert_quality(self, item):
         if item.quality < 50:
-            item.quality = item.quality + 1
+            item.quality += 1
             if item.name == concert:
                 if item.sell_in < 11 and item.quality <50:
-                        item.quality = item.quality + 1
+                        item.quality += 1
                 if item.sell_in < 6 and item.quality <50:
-                        item.quality = item.quality + 1
+                        item.quality += 1
